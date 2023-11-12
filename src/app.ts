bacb0122a87fac7e96d406744e987cd66be82764
Mac_Developer <araozangel842@gmail.com>
2023-11-12 14:14:21 +0200

    Adding cors configuration

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,12 @@ import availabilityRoutes from './routes/available';
 
 const app = express();
 
-app.use(cors())
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://frolicking-sfogliatella-537655.netlify.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 app.use(express.json());
 
 
